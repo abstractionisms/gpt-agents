4db1a767a44116e4b22f1d1810780ffccdc5acb0
scrobble <czeineld@gmail.com>
2023-12-18 20:59:46 -0800

    added /c functionality

--- dpy.py
+++ dpy.py
@@ -1,7 +1,7 @@
 import discord
 import requests
 import configparser
-import os 
+import os
 
 # Set the possible base directories for config.ini
 base_directories = [
@@ -26,15 +26,7 @@ for base_directory in base_directories:
         print(f"Error reading {config_file_path}: {e}")
         break
 
-# Access API keys from the configuration file
-api_keys = {}
-if 'API_KEYS' in config:
-    for key in config['API_KEYS']:
-        api_keys[key] = config.get('API_KEYS', key)
-
 # Use the API keys as needed in your script
-for key, value in api_keys.items():
-    print(f"{key}: {value}")
 api_key1 = config.get('API_KEYS', 'api_key1')
 api_key3 = config.get('API_KEYS', 'api_key3')  # Discord token
 
@@ -51,7 +43,7 @@ def chat_gpt(api_key, prompt, conversation_history=None):
     messages.append({'role': 'user', 'content': prompt})
 
     data = {
-        'model': 'gpt-4-0613',
+        'model': 'gpt-4-1106-preview',
         'messages': messages,
         'temperature': 0.5,
         'top_p': 1,
@@ -66,6 +58,41 @@ def chat_gpt(api_key, prompt, conversation_history=None):
     else:
         raise Exception(f"API request failed with status code {response.status_code}. Error details: {response.text}")
 
+def chat_cybersecurity(api_key, prompt, conversation_history=None):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+    messages = [{'role': 'system', 'content': 'I want you to act like a Cybersecurity expert. I will ask questions about network systems and command line scripting as I am a novice learning. Please provide easy to understand explanations and then after determining the issue, offer clear instructions.'}]
+
+    if conversation_history:
+        messages.extend(conversation_history)
+
+    messages.append({'role': 'user', 'content': prompt})
+
+    data = {
+        'model': 'gpt-4-1106-preview',
+        'messages': messages,
+        'temperature': 0.5,
+        'top_p': 1,
+        'max_tokens': 2560
+    }
+    response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=data)
+
+    if response.status_code == 200:
+        result = response.json()
+        message = result['choices'][0]['message']['content']
+        return message.strip()
+    else:
+        raise Exception(f"API request failed with status code {response.status_code}. Error details: {response.text}")
+
+# Command Handlers
+command_handlers = {
+    '/p': chat_gpt,
+    '/c': chat_cybersecurity,
+    # Add more commands and handlers as needed
+}
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -79,18 +106,19 @@ class MyClient(discord.Client):
         if not isinstance(message.channel, discord.channel.DMChannel):
             return
 
-        # respond only to '/a' command
-        if not message.content.startswith('/a'):
-            return
-
         # process the command
-        command = message.content[2:].strip()  # remove '/a' from the command
-        response = chat_gpt(api_key1, command)  # use the chat_gpt function
+        for prefix, handler in command_handlers.items():
+            if message.content.startswith(prefix):
+                command = message.content[len(prefix):].strip()
+                response = handler(api_key1, command)
+                break
+        else:
+            return  # Ignore messages that don't match any command prefix
 
         # Check if the response is too long
         if len(response) > 2000:
             # If it's too long, split it into chunks of 2000 characters
-            chunks = [response[i:i+2000] for i in range(0, len(response), 2000)]
+            chunks = [response[i:i + 2000] for i in range(0, len(response), 2000)]
             for chunk in chunks:
                 await message.channel.send(chunk)
         else:
